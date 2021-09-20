45148a8fe220664ece2318ecedefdc28759f201d
Gino16 <gino.francisco@hotmail.com>
2021-09-20 11:17:37 -0500

    Implementado EmailJS para formulario de contacto

--- js/main.js
+++ js/main.js
@@ -1,162 +1,162 @@
 // VER Y OCULTAR MENU
 const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
 
 // VER MENU
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
 }
 // OCULTAR MENU
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
 }
 
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu')
-    //    Cuando haga click en un elemento del menu, ocultaremos el menu
-    navMenu.classList.remove('show-menu')
+  const navMenu = document.getElementById('nav-menu')
+  //    Cuando haga click en un elemento del menu, ocultaremos el menu
+  navMenu.classList.remove('show-menu')
 }
 
 navLink.forEach(element => element.addEventListener('click', linkAction))
 
 // EFECTO ACORDION EN SKILLS
 const skillsContent = document.getElementsByClassName('skills__content'),
-    skillsHeader = document.querySelectorAll('.skills__header')
+  skillsHeader = document.querySelectorAll('.skills__header')
 
 function toggleSkills() {
-    let itemClass = this.parentNode.className
+  let itemClass = this.parentNode.className
 
-    for (let i = 0; i < skillsContent.length; i++) {
-        skillsContent[i].className = 'skills__content skills__close'
-    }
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills__content skills__close'
+  }
 
-    if (itemClass === 'skills__content skills__close') {
-        this.parentNode.className = 'skills__content skills__open'
-    }
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open'
+  }
 }
 
 skillsHeader.forEach((element) => {
-    element.addEventListener('click', toggleSkills)
+  element.addEventListener('click', toggleSkills)
 })
 
 /* QUALIFICATION TABS*/
 const tabs = document.querySelectorAll('[data-target]'),
-    tabContents = document.querySelectorAll('[data-content]')
+  tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.target)
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
 
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('qualification__active')
-        })
-        target.classList.add('qualification__active')
-
-        tabs.forEach(tab => {
-            tab.classList.remove('qualification__active')
-        })
-        tab.classList.add('qualification__active')
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active')
     })
+    target.classList.add('qualification__active')
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+  })
 })
 
 // SERVICES MODAL
 const modalViews = document.querySelectorAll('.services__modal'),
-    modalBtns = document.querySelectorAll('.services__button'),
-    modalCloses = document.querySelectorAll('.services__modal-close')
+  modalBtns = document.querySelectorAll('.services__button'),
+  modalCloses = document.querySelectorAll('.services__modal-close')
 
 let modal = function (modalClick) {
-    modalViews[modalClick].classList.add('active-modal')
+  modalViews[modalClick].classList.add('active-modal')
 }
 
 modalBtns.forEach((modalBtn, i) => {
-    modalBtn.addEventListener('click', () => {
-        modal(i)
-    })
+  modalBtn.addEventListener('click', () => {
+    modal(i)
+  })
 })
 
 modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener('click', () => {
-        modalViews.forEach((modalView) => {
-            modalView.classList.remove('active-modal')
-        })
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal')
     })
+  })
 })
 
 // SWIPER SLIDE PORTFOLIO
 let swiperPortfolio = new Swiper(".portfolio__container", {
-    cssMode: true,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-    },
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
 })
 
 // SWIPER SLIDE DYNAMIC
 let swiperTestimonial = new Swiper(".testimonial__container", {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 48,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true
-    },
-    breakpoints: {
-        568: {
-            slidesPerView: 2
-        }
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2
     }
+  }
 })
 
 // SCROLL SECTIONS ACTIVE LINK
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
-    const scrollY = window.pageYOffset
+  const scrollY = window.pageYOffset
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        let sectionId = current.getAttribute('id')
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    let sectionId = current.getAttribute('id')
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
 }
 
 window.addEventListener('scroll', scrollActive)
 
 // Change background header
 function scrollHeader() {
-    const nav = document.getElementById('header')
-    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+  const nav = document.getElementById('header')
+  if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 
 window.addEventListener('scroll', scrollHeader)
 
 // Show scroll up
 function scrollUp() {
-    const scrollUp = document.getElementById('scroll-up')
-    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  const scrollUp = document.getElementById('scroll-up')
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 
 window.addEventListener('scroll', scrollUp)
@@ -173,14 +173,37 @@ const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dar
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
 if (selectedTheme) {
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 themeButton.addEventListener('click', () => {
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
 
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+});
+
+(function () {
+  emailjs.init('user_QuPTGsbBPFfe1R88S47jJ')
+})();
+
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    this.contact_number.value = Math.random() * 100000 | 0;
+
+    emailjs.sendForm('service_hhxzn99', 'template_neujd7j', this)
+      .then(function () {
+        window.alert('Se ha enviado el correo exitosamente!');
+      }, function (error) {
+        window.alert('Lo siento, algo ha fallado. Contactame por mi correo electrónico o número de celular.');
+      });
+    document.getElementById('name').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('project').value = "";
+    document.getElementById('message').value = "";
+  });
+}
